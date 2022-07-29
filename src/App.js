ef14c2700e6e8f7fd7ab9b8a8ccdb865e4806c18
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-07-29 10:49:02 -0300

    Se creo carpeta Utils, y se movieron carpetas a esta nueva ubicación

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-unused-vars */
 import "./App.css";
-import Calendario from "./Components/calendario/Calendario";
+import Calendario from "./Components/Utils/calendario/Calendario";
 import Home from "./Components/home/Home";
 import Login from "./Components/login/Login";
 
 
 const App = () => {
   return (
-    
+
       <Home/>
 
   );
