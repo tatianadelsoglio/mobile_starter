623213abf2f02e2cb5cf999d1ed3e052657c9a51
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-08-01 12:20:38 -0300

    se creo carpeta layout, se creo archivo MainLayout.js, se re acomodo en el mismo archivo el contenido del archivo Home.js para realizar toda la reestructuración necesaria para que funcione correctamente el router

---
+++ src/Components/ui/layout/MainLayout.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Menu from "../menu/Menu";
+import Nav from "../navBar/Nav";
+
+const MainLayout = ({ children }) => {
+  return (
+    <>
+      <div className="vista_home_wrapper">
+        <div className="vista_home_content1" />
+        <div className="vista_home_content">
+          <div className="home_nav">
+            <Nav />
+          </div>
+          <div className="home_contenido">
+            {children}
+          </div>
+          <div className="home_menuInf">
+            <Menu />
+          </div>
+        </div>
+        <div className="vista_home_content1" />
+      </div>
+    </>
+  );
+};
+
+export default MainLayout;
