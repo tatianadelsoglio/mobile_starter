8089254ad1a722c862099d33f58d2262f4663775
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-08-02 11:03:00 -0300

    Se creo nueva carpeta nuevaTarea, con los archivos NuevaTarea.js y NuevaTarea.css, tambien se creo NuevaTareaView.js y se agrego el ruteo en AppRouter

--- src/Components/router/AppRouter.js
+++ src/Components/router/AppRouter.js
@@ -5,6 +5,7 @@ import ClientesView from "../../views/ClientesView";
 import DetalleTareaView from "../../views/DetalleTareaView";
 import HomeView from "../../views/HomeView";
 import LoginView from "../../views/LoginView";
+import NuevaTareaView from "../../views/NuevaTareaView";
 import TareasView from "../../views/TareasView";
 
 
@@ -29,6 +30,9 @@ const AppRouter = () => {
         </Route>
         <Route exact path="/detalletarea">
           <DetalleTareaView />
+        </Route>
+        <Route exact path="/nuevatarea">
+          <NuevaTareaView />
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ src/Components/ui/nuevaTarea/NuevaTarea.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NuevaTarea = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NuevaTarea;
