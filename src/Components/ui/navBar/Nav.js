0bee7bbc383b2130986b688009eec386c36112ff
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-08-02 10:08:05 -0300

    Se modifico iconos de NavBar, se cambio calendario por salida, se agrego funcionalidad de cerrar sesión y cascaron de modal para crear una nueva tarea

--- src/Components/ui/navBar/Nav.js
+++ src/Components/ui/navBar/Nav.js
@@ -1,16 +1,46 @@
 import React from "react";
-import { NavBar, Space } from "antd-mobile";
-import { CalendarOutline, AddCircleOutline } from "antd-mobile-icons";
+import { Modal, NavBar, Space } from "antd-mobile";
+import { UploadOutline, AddCircleOutline } from "antd-mobile-icons";
 import { useHistory } from "react-router-dom";
 
 const Nav = () => {
   let history = useHistory();
 
+  const handleModalSalir = () => {
+    history.push("/");
+  };
+
+  const handleModalCrearTarea = () => {
+    //history.push("/nuevatarea");
+    alert("Crear Nueva Tarea");
+  };
+
   const right = (
     <div style={{ fontSize: 24 }}>
       <Space style={{ "--gap": "16px" }}>
-        <CalendarOutline color="#56b43c" />
-        <AddCircleOutline color="#56b43c" />
+        <AddCircleOutline
+          color="#56b43c"
+          onClick={() =>
+            Modal.confirm({
+              title: "¿Crear una nueva tarea?",
+              cancelText: "Cancelar",
+              confirmText: "Crear",
+              onConfirm: handleModalCrearTarea,
+            })
+          }
+        />
+        <UploadOutline
+          style={{ transform: "rotate(90deg)" }}
+          color="#56b43c"
+          onClick={() =>
+            Modal.confirm({
+              title: "¿Cerrar Sesión?",
+              cancelText: "Cancelar",
+              confirmText: "Cerrar",
+              onConfirm: handleModalSalir,
+            })
+          }
+        />
       </Space>
     </div>
   );
