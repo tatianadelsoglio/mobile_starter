ceaf98c3acf0d36f936aaa759252a184e2d40922
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-08-02 11:53:42 -0300

    Se modifico el swipeAction para que se encuentren las opcienes del lado derecho unicamente

--- src/Components/ui/listaTareas/ListaTarea.js
+++ src/Components/ui/listaTareas/ListaTarea.js
@@ -19,57 +19,6 @@ const ListaTarea = () => {
             ref={ref}
             closeOnAction={false}
             closeOnTouchOutside={false}
-            leftActions={[
-              {
-                key: "cerrar",
-                text: "Cerrar Tarea",
-                color: "danger",
-
-                onClick: async () => {
-                  await Dialog.confirm({
-                    content: "¿Cerrar Tarea?",
-                    cancelText: "Cancelar",
-                    confirmText: "Aceptar",
-                  });
-                  ref.current?.close();
-                },
-              },
-            ]}
-          >
-            <List.Item
-              description="Adrian Sabo"
-              extra="29-07-2022"
-              clickable
-              onClick={() => {
-                history.push("/detalletarea");
-              }}
-            >
-              <Ellipsis
-                direction="end"
-                content="Llamar a Adrian, conversar sobre nuevos insumos"
-              />
-            </List.Item>
-          </SwipeAction>
-          <SwipeAction
-            ref={ref}
-            closeOnAction={false}
-            closeOnTouchOutside={false}
-            leftActions={[
-              {
-                key: "cerrar",
-                text: "Cerrar Tarea",
-                color: "danger",
-
-                onClick: async () => {
-                  await Dialog.confirm({
-                    content: "¿Cerrar Tarea?",
-                    cancelText: "Cancelar",
-                    confirmText: "Aceptar",
-                  });
-                  ref.current?.close();
-                },
-              },
-            ]}
             rightActions={[
               {
                 key: "editar",
@@ -85,48 +34,62 @@ const ListaTarea = () => {
                   ref.current?.close();
                 },
               },
+              {
+                key: "cerrar",
+                text: "Cerrar Tarea",
+                color: "danger",
+
+                onClick: async () => {
+                  await Dialog.confirm({
+                    content: "¿Cerrar Tarea?",
+                    cancelText: "Cancelar",
+                    confirmText: "Aceptar",
+                  });
+                  ref.current?.close();
+                },
+              },
             ]}
           >
-            <List.Item
-              description="Horacio Mercol"
-              extra="29-07-2022"
-              
-            >
-              <Ellipsis direction="end" content="Visitar Campo Oeste" />
+            <List.Item description="Adrian Sabo" extra="29-07-2022">
+              <Ellipsis
+                direction="end"
+                content="Llamar a Adrian, conversar sobre nuevos insumos"
+              />
             </List.Item>
           </SwipeAction>
+          
+          <List.Item description="Horacio Mercol" extra="29-07-2022">
+            <Ellipsis direction="end" content="Visitar Campo Oeste" />
+          </List.Item>
 
-          <List.Item description="Adrian Sabo" extra="29-07-2022" >
+          <List.Item description="Adrian Sabo" extra="29-07-2022">
             <Ellipsis direction="end" content="Venta de Soja" />
           </List.Item>
-          <List.Item
-            description="Adrian Sabo"
-            extra="30-07-2022"
-          >
+          <List.Item description="Adrian Sabo" extra="30-07-2022">
             <Ellipsis
               direction="end"
               content="Venta de Maíz para temporada 2022"
             />
           </List.Item>
-          <List.Item description="Adrian Sabo" extra="30-07-2022" >
+          <List.Item description="Adrian Sabo" extra="30-07-2022">
             <Ellipsis
               direction="end"
               content="Llamar a Adrian, conversar sobre nuevos insumos"
             />
           </List.Item>
-          <List.Item description="Horacio Mercol" extra="31-07-2022" >
+          <List.Item description="Horacio Mercol" extra="31-07-2022">
             <Ellipsis direction="end" content="Visitar Campo Oeste" />
           </List.Item>
-          <List.Item description="Adrian Sabo" extra="31-07-2022" >
+          <List.Item description="Adrian Sabo" extra="31-07-2022">
             <Ellipsis direction="end" content="Venta de Soja" />
           </List.Item>
-          <List.Item description="Jorge Mayorga" extra="01-08-2022" >
+          <List.Item description="Jorge Mayorga" extra="01-08-2022">
             <Ellipsis
               direction="end"
               content="Llamar a Jorge para Venta de Herbicidas"
             />
           </List.Item>
-          <List.Item description="Aida Campos" extra="01-08-2022" >
+          <List.Item description="Aida Campos" extra="01-08-2022">
             <Ellipsis direction="end" content="Venta de Maíz" />
           </List.Item>
         </List>
