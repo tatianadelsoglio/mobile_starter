c024f12935a7cc190a72df85547f6cd1cac2dd95
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-08-22 09:15:46 -0300

    se agrego botones con funcionalidad para llamar y enviar mail

--- src/Components/ui/infoCliente/InfoCliente.js
+++ src/Components/ui/infoCliente/InfoCliente.js
@@ -1,4 +1,4 @@
-import { Card } from "antd-mobile";
+import { Button, Card } from "antd-mobile";
 import React from "react";
 import { PhonebookOutline, MailOutline } from "antd-mobile-icons";
 import "./InfoCliente.css";
@@ -27,9 +27,10 @@ const InfoCliente = ({ clienteSelect }) => {
     },
   ];
 
-  const Llamar = (cel) => {
-    console.log("Llamando a ...", cel);
-  };
+  let contactoPrueba = 3533418378;
+  function Llamar(e) {
+    window.location.assign(e.getAttribute("href"));
+  }
 
   return (
     <>
@@ -38,10 +39,10 @@ const InfoCliente = ({ clienteSelect }) => {
           <div className="div_emp">
             <p className="p_infoEmpresa"> {clienteSelect.empresa} </p>
             <p className="p_info">
-              <PhonebookOutline />{" "}{clienteSelect.telefono}
+              <PhonebookOutline /> {clienteSelect.telefono}
             </p>
             <p className="p_info">
-              <MailOutline />{" "}{clienteSelect.email}
+              <MailOutline /> {clienteSelect.email}
             </p>
           </div>
         </Card>
@@ -50,17 +51,42 @@ const InfoCliente = ({ clienteSelect }) => {
           <Card className="card_contactos">
             <div className="div_contacto_lista">
               {contactos.map((contacto) => (
-                <Card className="border" onClick={() => Llamar(contacto.telefono_contacto)}>
+                <Card
+                  className="border"
+                  onClick={() => Llamar(contacto.telefono_contacto)}
+                >
                   <div>
-                    <p className="p_infoContacto">
-                      {contacto.nombre_contacto}
+                    <p className="p_infoContacto">{contacto.nombre_contacto}</p>
+                    <div className="div_content_enlace">
+                      <p style={{ display: "inline", marginRight: "10px" }}>
+                        <a
+                          className="numCel"
+                          href={"tel:+54" + contacto.telefono_contacto}
+                        >
+                          <div className="div_enlace">
+                            <PhonebookOutline style={{ fontSize: "3rem" }} />
+                            <p style={{ margin: "0px" }}>Llamar</p>
+                          </div>
+                        </a>
+                      </p>
+                      <p style={{ display: "inline" }}>
+                        <a
+                          className="numCel"
+                          href={"mailto:" + contacto.email_contacto}
+                        >
+                          <div className="div_enlace">
+                            <MailOutline style={{ fontSize: "3rem" }} />
+                            <p style={{ margin: "0px" }}>Enviar</p>
+                          </div>
+                        </a>
+                      </p>
+                    </div>
+
+                    <p className="p_info">
+                      <PhonebookOutline /> {contacto.telefono_contacto}
                     </p>
                     <p className="p_info">
-                      <PhonebookOutline />{" "}
-                      <a className="numCel" href="tel:+543535639705">{contacto.telefono_contacto}</a>
-                    </p>
-                    <p className="p_info">
-                      <MailOutline />{" "}{contacto.email_contacto}
+                      <MailOutline /> {contacto.email_contacto}
                     </p>
                   </div>
                 </Card>
