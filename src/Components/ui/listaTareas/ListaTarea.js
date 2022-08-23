6a6736571d876e5437b8181d8041cc1d745d48d4
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-08-23 10:46:06 -0300

    cambios home tareas

--- src/Components/ui/listaTareas/ListaTarea.js
+++ src/Components/ui/listaTareas/ListaTarea.js
@@ -130,59 +130,135 @@ const ListaTarea = ({ ItemListaTarea }) => {
                     closeOnTouchOutside={false}
                     rightActions={rightActions}
                   >
-                    <div className="tarea-negocio-contenedor ">
+                    <div className="tarea-negocio-contenedor borde_contenedor">
                       <div className="tarea-negocio-linea-superior">
-                        <p className="tarea-negocio-titulo">
+                        <p className="tarea-negocio-titulo margenElip">
                           <Ellipsis
-                            style={{ fontWeight: "bold", width:"5em" }}
+                            style={{ fontWeight: "bold", width: "18rem", fontSize:"16px" }}
                             direction="end"
                             content={ItemListaTarea.descripcion}
                           />
                         </p>
-                        <CheckOutline
-                          style={{
-                            color: "#00B33C",
-                            marginRight: "5px",
-                            fontSize: "1rem",
-                          }}
-                        />
                       </div>
-                      <div className="tarea-negocio-linea-inferior">
-                        <p className="tarea-negocio-fecha">
-                          {handleHora(ItemListaTarea.fechaHora)}
-                        </p>
-                        {ItemListaTarea.empresa ? (
-                          <div className="tarea-negocio-item">
-                            <ShopbagOutline style={{ color: "#00B33C" }} />{" "}
-                            <p className="tarea-negocio-contacto">
-                              {ItemListaTarea.empresa}
+                      <div className="tarea-negocio-linea-inferior flexColumn">
+                        <div className="tarea-negocio-linea-inferior interlineado">
+                          {ItemListaTarea.empresa ? (
+                            <div className="tarea-negocio-item">
+                              <ShopbagOutline style={{ color: "#00B33C" }} />{" "}
+                              <p className="tarea-negocio-contacto">
+                                {ItemListaTarea.empresa}
+                              </p>
+                            </div>
+                          ) : (
+                            ""
+                          )}
+                          {ItemListaTarea.empresa ? (
+                            <div className="tarea-negocio-item">
+                              <InformationCircleOutline
+                                style={{ color: "#00B33C" }}
+                              />{" "}
+                              <p className="tarea-negocio-tipoTarea">
+                                {ItemListaTarea.empresa}
+                              </p>
+                            </div>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                        <div className="tarea-negocio-linea-inferior interlineado">
+                          <div className="tarea-contenedor-horario">
+                            <ClockCircleOutline
+                              style={{ color: "white", fontSize: "0.8rem" }}
+                            />
+                            <p className="texto-tarea-horario">
+                              {handleFechaVer(ItemListaTarea.fechaHora)}
+                            </p>
+                            <p className="texto-tarea-horario">
+                              {handleHora(ItemListaTarea.fechaHora)} hs
                             </p>
                           </div>
-                        ) : (
-                          ""
-                        )}
-                        {ItemListaTarea.empresa ? (
-                          <div className="tarea-negocio-item">
-                            <InformationCircleOutline
-                              style={{ color: "#00B33C" }}
-                            />{" "}
-                            <p className="tarea-negocio-tipoTarea">
-                              {ItemListaTarea.empresa}
-                            </p>
+
+                          <div className="tarea-negocio-linea-inferior interlineado" style={{marginLeft:"5px", paddingBottom:"10px"}}>
+
+                            <div>
+                              {ItemListaTarea.prioridad === "ALTA" ? (
+                                <div
+                                  style={{
+                                    height: "20px",
+                                    width: "40px",
+                                    fontSize: "12px",
+                                    backgroundColor: "#da4453",
+                                    color: "white",
+                                    border: "solid 1px #da4453",
+                                    borderRadius: "4px",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                  }}
+                                >
+                                  ALTA
+                                </div>
+                              ) : null}
+                              {ItemListaTarea.prioridad === "MEDIA" ? (
+                                <div
+                                  style={{
+                                    height: "20px",
+                                    width: "40px",
+                                    fontSize: "12px",
+                                    backgroundColor: "#f7c560",
+                                    color: "white",
+                                    border: "solid 1px #f7c560",
+                                    borderRadius: "4px",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                  }}
+                                >
+                                  MEDIA
+                                </div>
+                              ) : null}
+                              {ItemListaTarea.prioridad === "BAJA" ? (
+                                <div
+                                  style={{
+                                    height: "20px",
+                                    width: "40px",
+                                    fontSize: "12px",
+                                    backgroundColor: "#8cc152",
+                                    color: "white",
+                                    border: "solid 1px #8cc152",
+                                    borderRadius: "4px",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                  }}
+                                >
+                                  BAJA
+                                </div>
+                              ) : null}
+                            </div>
+                            <div>
+                              <p>
+                                <div
+                                  style={{
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    border: "solid 1px #f4f4f4",
+                                    height: "20px",
+                                    width: "auto",
+                                    fontSize: "12px",
+                                    color:"#7cb305",
+                                    borderColor:"#eaff8f",
+                                    backgroundColor:"#fcffe6",
+                                    
+                                    borderRadius: "4px",
+                                  }}
+                                >
+                                  NEGOCIO
+                                </div>
+                              </p>
+                            </div>
                           </div>
-                        ) : (
-                          ""
-                        )}
-                        <div className="tarea-contenedor-horario">
-                          <ClockCircleOutline
-                            style={{ color: "white", fontSize: "0.8rem" }}
-                          />
-                          <p className="texto-tarea-horario">
-                            {handleFecha(ItemListaTarea.fechaHora)}
-                          </p>
-                          <p className="texto-tarea-horario">
-                            {handleHora(ItemListaTarea.fechaHora)} hs
-                          </p>
                         </div>
                       </div>
                     </div>
