24ec87da94678f177c93a182517a24d0747dc02e
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-08-25 12:57:16 -0300

    agrego detalle prioridad en lista de tareas calendario, se agrego funcionalidad de redirección a editar tarea desde lista tarea de calendario

--- src/Components/ui/listaCalendario/TareasCalendario.js
+++ src/Components/ui/listaCalendario/TareasCalendario.js
@@ -1,24 +1,28 @@
-import {
-  Dialog,
-  Ellipsis,
-  List,
-  Modal,
-  SwipeAction,
-} from "antd-mobile";
+import { Dialog, Ellipsis, List, Modal, SwipeAction } from "antd-mobile";
 import React, { useRef } from "react";
 import { useHistory } from "react-router-dom";
-import { EditSOutline, CheckOutline} from "antd-mobile-icons";
+import { EditSOutline, CheckOutline, ShopbagOutline } from "antd-mobile-icons";
 import moment from "moment";
 import "./TareasCalendario.css";
 
-const ListaCalendario = ({ItemListaTarea}) => {
+const ListaCalendario = ({ ItemListaTarea }) => {
   let history = useHistory();
 
   const ref = useRef(null);
 
-  const handleModalDetalleTarea = () => {
-    history.push("/detalletarea");
+
+  const handleModalDetalleTarea = (tareaP) => {
+    let cliente = ItemListaTarea.filter((tarea) => tarea.id === tareaP.id);
+
+    return history.push({
+      pathname: `/detalletarea/${tareaP.id}`,
+      state: { ...cliente },
+    });
   };
+
+  // const handleModalDetalleTarea = () => {
+  //   history.push("/detalletarea");
+  // };
 
   const handleModalCerrar = () => {
     Modal.alert({
@@ -35,73 +39,148 @@ const ListaCalendario = ({ItemListaTarea}) => {
     });
   };
 
-  const rightActions = [
-    {
-      key: "editar",
-      text: <EditSOutline />,
-      color: "#2bc4e3",
-      onClick: async () => {
-        await Dialog.confirm({
-          content: "¿Editar Tarea?",
-          cancelText: "Cancelar",
-          confirmText: "Aceptar",
-          onConfirm: handleModalDetalleTarea,
-        });
-        ref.current?.close();
-      },
-    },
-    {
-      key: "cerrar",
-      text: <CheckOutline />,
-      color: "primary",
-      onClick: async () => {
-        await Dialog.confirm({
-          content: "¿Cerrar Tarea?",
-          cancelText: "Cancelar",
-          confirmText: "Aceptar",
-          onConfirm: handleModalCerrar,
-        });
-        ref.current?.close();
-      },
-    },
-  ];
-
   const handleHora = (val) => {
     let hora = moment(val).format("LT");
     return hora;
   };
-
 
   return (
     <>
       <div className="div_lista_tareas">
         {ItemListaTarea.map((ItemListaTarea) => (
           <div>
-            <List>  
+            <List>
               <SwipeAction
                 ref={ref}
                 closeOnAction={false}
                 closeOnTouchOutside={false}
-                rightActions={rightActions}
+                rightActions={[
+                  {
+                    key: "editar",
+                    text: <EditSOutline />,
+                    color: "#2bc4e3",
+                    onClick: () => {
+                      handleModalDetalleTarea(ItemListaTarea);
+                    },
+                  },
+                  {
+                    key: "cerrar",
+                    text: <CheckOutline />,
+                    color: "primary",
+                    onClick: async () => {
+                      await Dialog.confirm({
+                        content: "¿Cerrar Tarea?",
+                        cancelText: "Cancelar",
+                        confirmText: "Aceptar",
+                        onConfirm: handleModalCerrar,
+                      });
+                      ref.current?.close();
+                    },
+                  },
+                ]}
               >
                 <List.Item
                   className="lista_tarea_calendar"
                   key={ItemListaTarea.key}
-                  description={ItemListaTarea.usu_nombre}
-                  onClick={() =>
-                    Modal.show({
-                      title: ItemListaTarea.description,
-                      content: ItemListaTarea.content,
-                      closeOnMaskClick: true,
-                    })
+                  description={
+                    <div className="prioridad_cliente">
+                      <div>
+                        {ItemListaTarea.prioridad === "ALTA" ? (
+                          <div
+                            style={{
+                              height: "20px",
+                              width: "40px",
+                              fontSize: "12px",
+                              backgroundColor: "#da4453",
+                              color: "white",
+                              border: "solid 1px #da4453",
+                              borderRadius: "4px",
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center",
+                              padding: "2px 5px",
+                            }}
+                          >
+                            ALTA
+                          </div>
+                        ) : null}
+                        {ItemListaTarea.prioridad === "MEDIA" ? (
+                          <div
+                            style={{
+                              height: "20px",
+                              width: "40px",
+                              fontSize: "12px",
+                              backgroundColor: "#f7c560",
+                              color: "white",
+                              border: "solid 1px #f7c560",
+                              borderRadius: "4px",
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center",
+                              padding: "2px 5px",
+                            }}
+                          >
+                            MEDIA
+                          </div>
+                        ) : null}
+                        {ItemListaTarea.prioridad === "BAJA" ? (
+                          <div
+                            style={{
+                              height: "20px",
+                              width: "40px",
+                              fontSize: "12px",
+                              backgroundColor: "#8cc152",
+                              color: "white",
+                              border: "solid 1px #8cc152",
+                              borderRadius: "4px",
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center",
+                              padding: "2px 5px",
+                            }}
+                          >
+                            BAJA
+                          </div>
+                        ) : null}
+                      </div>
+                      <div className="icon_cliente">
+                        <ShopbagOutline style={{ color: "#00B33C" }} />{" "}
+                        <p className="p_cliente">
+                          {ItemListaTarea.cliente}
+                        </p>
+                      </div>
+                    </div>
                   }
                 >
-                  <div style={{display:"flex", flexDirection:"row", justifyContent:"space-between", alignItems:"center"}}>
-                    <div style={{display:"flex", justifyContent:"flex-start", alignItems:"center"}}>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "flex-start",
+                        alignItems: "center",
+                      }}
+                    >
                       {handleHora(ItemListaTarea.fechaHora)}
                     </div>
-                    <div className="div_tarea_calendar" style={{display:"flex", justifyContent:"flex-end", alignItems:"center"}}>
-                      <Ellipsis direction="end" content={ItemListaTarea.descripcion} />
+                    <div
+                      className="div_tarea_calendar"
+                      style={{
+                        display: "flex",
+                        justifyContent: "flex-end",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Ellipsis
+                        direction="end"
+                        content={ItemListaTarea.asunto}
+                      />
                     </div>
                   </div>
                 </List.Item>
@@ -111,7 +190,6 @@ const ListaCalendario = ({ItemListaTarea}) => {
         ))}
 
         <div style={{ height: "40px" }}></div>
-  
       </div>
     </>
   );
