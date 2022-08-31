da26eb4389ad39814dfaa90b9d05daf09fc94efc
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-08-31 11:51:10 -0300

    se modifico estilos de lista de la vista cliente y dentro de cliente tambien en cliente/negocio, se fixio capsules y se creo contenedores scrolleables

--- src/Components/ui/clientes/cliente-individual/ClienteIndividual.js
+++ src/Components/ui/clientes/cliente-individual/ClienteIndividual.js
@@ -1,64 +1,63 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { CapsuleTabs, SwipeAction } from "antd-mobile"
+import { CapsuleTabs, SwipeAction } from "antd-mobile";
 import { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { GlobalContext } from "../../../context/GlobalContext";
-import InfoCliente from "../../infoCliente/InfoCliente"
-import { ListaNegocios } from "../../negocios/ListaNegocios"
+import InfoCliente from "../../infoCliente/InfoCliente";
+import { ListaNegocios } from "../../negocios/ListaNegocios";
 import { TareaNegocio } from "../../tareaNegocio/TareaNegocio";
 import "./clienteIndividual.css";
 
-
 export const ClienteIndividual = () => {
-
   const { listaTareas } = useContext(GlobalContext);
 
   const location = useLocation();
 
   const [cliente, setCliente] = useState({});
 
-  const [ tareasXCliente, setTareasXCliente ] = useState();
+  const [tareasXCliente, setTareasXCliente] = useState();
 
   const tareasHandler = () => {
-    setTareasXCliente(listaTareas.filter(tarea => tarea.cliente === cliente.empresa));
-  }
-
+    setTareasXCliente(
+      listaTareas.filter((tarea) => tarea.cliente === cliente.empresa)
+    );
+  };
 
   useEffect(() => {
     const clienteSelect = location.state[0];
 
     setCliente(clienteSelect);
-
   }, []);
 
   useEffect(() => {
     tareasHandler();
-
   }, [cliente]);
 
-  useEffect(() => {
-
-  }, [tareasXCliente]);
+  useEffect(() => {}, [tareasXCliente]);
 
   return (
     <div className="contenedor-cliente-individual">
-        <CapsuleTabs>
-            <CapsuleTabs.Tab title="Info" key="1">
-              <InfoCliente clienteSelect={cliente}/>
-            </CapsuleTabs.Tab>
-            <CapsuleTabs.Tab title="Tareas" key="2">
-              {tareasXCliente?.map( tarea => {
-                return(
-                  <SwipeAction style={{marginLeft:"-3px"}}>
-                    <TareaNegocio tarea={tarea} origen = "ListaTareas"/>
-                  </SwipeAction>
-                )
-              })}
-            </CapsuleTabs.Tab>
-            <CapsuleTabs.Tab title="Negocios" key="3">
-              <ListaNegocios />
-            </CapsuleTabs.Tab>
-        </CapsuleTabs>
+      <CapsuleTabs>
+        <CapsuleTabs.Tab title="Info" key="1">
+            <InfoCliente clienteSelect={cliente} />
+        </CapsuleTabs.Tab>
+        <CapsuleTabs.Tab title="Tareas" key="2">
+          <div className="div_lista_l">
+            {tareasXCliente?.map((tarea) => {
+              return (
+                <SwipeAction style={{ marginLeft: "-3px" }}>
+                  <TareaNegocio tarea={tarea} origen="ListaTareas" />
+                </SwipeAction>
+              );
+            })}
+          </div>
+        </CapsuleTabs.Tab>
+        <CapsuleTabs.Tab title="Negocios" key="3">
+          <div className="div_lista_l">
+            <ListaNegocios />
+          </div>
+        </CapsuleTabs.Tab>
+      </CapsuleTabs>
     </div>
-  )
-}
+  );
+};
