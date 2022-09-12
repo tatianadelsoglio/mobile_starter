3180c3e0b59c57d3d05b8422c7ff1a3ebf58b2a0
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-09-12 12:32:29 -0300

    se creo modifico estetica de cuando hay o no hay contactos

--- src/Components/ui/infoCliente/InfoCliente.js
+++ src/Components/ui/infoCliente/InfoCliente.js
@@ -17,12 +17,13 @@ const InfoCliente = ({ clienteSelect }) => {
   });
 
   useEffect(() => {
-    if (data) {
+    if (data && data !== []) {
       console.log(data);
       setContact(data.getContactosResolver)
-    }
+    } else {setContact("")}
   }, [data]);
 
+  console.log(contact)
 
   return (
     <div className="div_contenedor_info">
@@ -56,48 +57,48 @@ const InfoCliente = ({ clienteSelect }) => {
 
       <div className="div_contacto2">
         <Card title="CONTACTOS" className="card_contactos">
-          <div className="div_contacto_lista">
-            {contact.map((contact) => (
-              <Card className="border">
-                <Card className="cards_internas">
-                  <div
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      alignItems: "flex-start",
-                    }}
-                  >
-                    <div>
-                      <p className="p_infoContacto">
-                        {contact.con_nombre}
-                      </p>
+            <div className="div_contacto_lista">
+              {contact && contact.length !== 0 ? (contact.map((contact) => (
+                <Card className="border">
+                  <Card className="cards_internas">
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                      }}
+                    >
+                      <div>
+                        <p className="p_infoContacto">
+                          {contact.con_nombre}
+                        </p>
+                      </div>
+
+                      {contact.con_telefono1 && contact.con_telefono1 !== "" ? (
+                        <a
+                          className="numCel p_info"
+                          href={"tel:+54" + contact.con_telefono1}
+                        >
+                          <PhonebookOutline /> {contact.con_telefono1}
+                        </a>
+
+                      ) : (<p className="numCel p_info" style={{marginLeft:"0px"}}> <PhonebookOutline /> - </p>)}
+
+                      {contact.con_email1 && contact.con_email1 !== "" ? (
+                        <a
+                          className="numCel p_info"
+                          href={"mailto:" + contact.con_email1}
+                        >
+                          <MailOutline /> {contact.con_email1}
+                        </a>
+
+                      ) : (<p className="numCel p_info" style={{marginLeft:"0px"}}> <MailOutline /> - </p>)} 
+
                     </div>
-
-                    {contact.con_telefono1 && contact.con_telefono1 !== "" ? (
-                      <a
-                        className="numCel p_info"
-                        href={"tel:+54" + contact.con_telefono1}
-                      >
-                        <PhonebookOutline /> {contact.con_telefono1}
-                      </a>
-
-                    ) : (<p className="numCel p_info" style={{marginLeft:"0px"}}> <PhonebookOutline /> - </p>)}
-
-                    {contact.con_email1 && contact.con_email1 !== "" ? (
-                      <a
-                        className="numCel p_info"
-                        href={"mailto:" + contact.con_email1}
-                      >
-                        <MailOutline /> {contact.con_email1}
-                      </a>
-
-                    ) : (<p className="numCel p_info" style={{marginLeft:"0px"}}> <MailOutline /> - </p>)} 
-
-                  </div>
+                  </Card>
                 </Card>
-              </Card>
-            ))}
-          </div>
+              ))):(<Empty/>)}
+            </div>
         </Card>
       </div>
     </div>
