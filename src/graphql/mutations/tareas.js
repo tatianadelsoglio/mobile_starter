4192db329948328cae9227a4671f8c6a7f2aed66
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-09-16 13:27:30 -0300

    implementacion mutation new tarea iframe, corregir error al enviarlo

--- src/graphql/mutations/tareas.js
+++ src/graphql/mutations/tareas.js
@@ -1,18 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const NEW_TAREA = gql`
-  mutation newTareaIframe(
-    $inputTarea: tareaInput
-    $inputNota: notaInput
-    $inputAdjunto: uploadInput
-    $usuAsig: Int
-  ) {
-    newTareaIframeResolver(
-      inputTarea: $inputTarea
-      inputNota: $inputNota
-      inputAdjunto: $inputAdjunto
-      usuAsig: $usuAsig
-    )
+  mutation newTareaIframe($inputTarea: tareaInput,$inputNota: notaInput,$inputAdjunto: uploadInput,$usuAsig: Int) {
+    newTareaIframeResolver(inputTarea: $inputTarea,inputNota: $inputNota,inputAdjunto: $inputAdjunto,usuAsig: $usuAsig)
   }
 `;
 
