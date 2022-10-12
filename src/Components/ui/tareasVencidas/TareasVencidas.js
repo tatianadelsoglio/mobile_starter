5567120d48761cea8a0a7da3b79f3eb51e54f170
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-10-12 10:54:20 -0300

    se elimino spinner en tareasVencidas.js

--- src/Components/ui/tareasVencidas/TareasVencidas.js
+++ src/Components/ui/tareasVencidas/TareasVencidas.js
@@ -2,14 +2,9 @@ import moment from "moment";
 import { useEffect, useState } from "react";
 import QueryResult from "../../queryResult/QueryResult";
 import ListaTarea from "../listaTareas/ListaTarea";
-// import ReactSpinnerTimer from "react-spinner-timer";
-// import { AutoSizer, List } from "react-virtualized";
-import { TailSpin } from "react-loader-spinner";
-import { FixedSizeList } from "react-window";
 
 export const TareasVencidas = ({tareasParametro, error, loading}) => {
   const [tareas, setTareas] = useState();
-  // const [estado] = useState(1);
 
   const ordenarDatos = (tareas) => {
     let tareasOrdenadas;
@@ -28,28 +23,6 @@ export const TareasVencidas = ({tareasParametro, error, loading}) => {
       setTareas(tareasOrdenadas);
     }
   };
-  /* INTENTO 3000 REACT LISTA VIRTUAL*/
-  // console.log("Tareas vencidas: ", tareas.length);
-
-  const getItemSize = (index) => tareas[index];
-
-  const row = () => {
-    <>
-      <QueryResult loading={loading} error={error} data={tareas}>
-        <FixedSizeList
-          height={500}
-          width={500}
-          itemSize={getItemSize}
-          itemCount={tareas.length}
-        >
-          <div className="div_lista">
-            <ListaTarea itemListaTarea={tareas} />
-          </div>
-        </FixedSizeList>
-      </QueryResult>
-    </>;
-  };
-  /* INTENTO 3000 REACT LISTA VIRTUAL*/
 
   useEffect(() => {
     if (tareasParametro) {
@@ -57,14 +30,6 @@ export const TareasVencidas = ({tareasParametro, error, loading}) => {
     }
   }, [tareasParametro]);
 
-  const [timeOff, setTimeOff] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeOff(false);
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
 
   return (
     <QueryResult loading={loading} error={error} data={tareas}>
