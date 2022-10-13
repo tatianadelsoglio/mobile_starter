8caa6e68a5f7aac491b08eac59a8c633013f509f
agustinantonucci <agustinantonucci@gmail.com>
2022-10-13 16:15:34 -0300

    refresh en tareas

--- src/Components/ui/tareaNegocio/TareaNegocio.js
+++ src/Components/ui/tareaNegocio/TareaNegocio.js
@@ -64,7 +64,7 @@ export const TareaNegocio = ({ tarea, origen = "" }) => {
           <CheckOutline
             style={{
               fontSize: 64,
-              color: "var(--adm-color-primary)",
+              color: "#56b43c",
             }}
           />
         ),
